f99831aa2fd6431a99f663d603070de36d9a5127
jake-the-cake <askjake331@gmail.com>
2022-08-23 12:49:10 -0400

    adjust header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,6 +2,9 @@ import React, { FC } from 'react'
 
 export const HeaderComponent: FC = () => {
   return (
-    <div className="page__header">Resume Farm</div>
+    <div className="page__header">
+      <div className="page__header--title">Resume Farm</div>
+      <div className="page__header--menu">...</div>
+    </div>
   )
 }
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { FC } from 'react'
 
-export const Dashboard = () => {
+export const Dashboard: FC = () => {
   return (
     <div className="page">Homepage</div>
   )
