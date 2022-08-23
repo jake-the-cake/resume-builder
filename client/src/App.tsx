0591e1acfec2ffb1c63a5ef25925945cd6b4c4a9
jake-the-cake <askjake331@gmail.com>
2022-08-23 12:42:55 -0400

    add routing to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { FooterComponent as Footer} from './components/Footer';
 import { HeaderComponent as Header } from './components/Header';
 import { RegistrationForm } from './components/RegistrationForm';
+import { Route, Routes } from 'react-router-dom'
+import { Dashboard } from './pages/Dashboard';
 
 function App() {
   return (
     <>
       <Header />
       <div className="page__content">
-        <RegistrationForm />
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/register' element={<RegistrationForm />} />
+        </Routes>
       </div>
       <Footer />
     </>
